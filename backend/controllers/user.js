ef713c35e86581026a1b3dbeb82678f58bd67761
Poph-Digital <propjhh>
2021-10-12 00:32:10 +0200

    user create add waterfall async

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,7 +1,11 @@
 const bcrypt = require('bcrypt');
 const jwtUtils = require('../utils/jwt.utils');
+const asyncLib = require('async');
 
 const User = require('../models');
+
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}$/;
 
 //Inscription
 exports.signup = (req, res, next) => {
@@ -11,42 +15,76 @@ exports.signup = (req, res, next) => {
     var password = req.body.password;
     var bio = req.body.bio;
 
-    if (email == null || username == null || password == null) {
+    if (email == null || firstName == null || lastName == null || password == null) {
         return res.status(400).json({ 'error': 'missing parameters' });
     }
 
-    models.User.findOne({
-            attribute: ['email'],
-            where: { email: email }
-        })
-        .then(function(userFound) {
+    //max length firstname
+    if (firstName.length > 13 || firstName.length < 3) {
+        return res.status(400).json({ 'error': 'wrong firstname (must be length 3 - 13' });
+    }
+    //max length lastname
+    if (lastName.length > 24 || lastName.length < 3) {
+        return res.status(400).json({ 'error': 'wrong lastname (must be length 3 - 24' });
+    }
+    //regex for valid the email
+    if (!EMAIL_REGEX.test(email)) {
+        return res.status(400).json({ 'error': 'email is not valid' });
+    }
+    //regex for valid the password
+    if (!PASSWORD_REGEX.test(password)) {
+        return res.status(400).json({ 'error': 'password invalid (must length 8 - 16 and include 1 uppercase, 1 lowercase and 1 number' });
+    }
+
+    asyncLib.waterfall([
+        function(done) {
+            models.User.findOne({
+                    attributes: ['email'],
+                    where: { email: email }
+                })
+                .then(function(userFound) {
+                    done(null, userFound);
+                })
+                .catch(function(err) {
+                    return res.status(500).json({ 'error': 'unable to verify user' });
+                });
+        },
+        function(userFound, done) {
             if (!userFound) {
                 bcrypt.hash(password, 5, function(err, bcryptedPassword) {
-                    var newUser = models.User.create({
-                            email: email,
-                            firstName: firstName,
-                            lastname: lastname,
-                            password: password,
-                            bio: bio,
-                            isAdmin: 0
-                        })
-                        .then(function(newUser) {
-                            return res.status(201).json({
-                                'userId': newUser.id
-                            })
-                        })
-                        .catch(function(err) {
-                            return res.status(500).json({ 'error': 'cannot add user' });
-                        });
+                    done(null, userFound, bcryptedPassword);
                 });
             } else {
-                return res.status(409).json({ 'error': 'user already exist' });
+                return res.status(409).json({ 'error': 'user already exist' })
             }
-        })
-        .catch(function(err) {
-            return res.status(500).json({ 'error': 'unable to verify user' });
-        })
+        },
+        function(userFound, bcryptedPassword, done) {
+            var newUser = mdels.User.create({
+                    email: email,
+                    firstName: firstName,
+                    lastName: firstName,
+                    password: bcryptedPassword,
+                    bio: bio,
+                    isAdmin: 0
+                })
+                .then(function(newUser) {
+                    done(newUser);
+                })
+                .catch(function(err) {
+                    return res.status(500).json({ 'error': 'cannot add user' });
+                });
+        }
+    ], function(newUser) {
+        if (newUser) {
+            return res.status(201).json({
+                'userId': newUser.id
+            });
+        } else {
+            return res.status(500).json({ 'error': 'cannot add user' });
+        }
+    });
 };
+
 
 //Connexion
 exports.login = (req, res, next) => {
