fcc9e8b0a3083953a60ef1b50f3819c79c8039f0
Poph-Digital <propjhh>
2021-10-15 13:30:19 +0200

    index content

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,11 +1,18 @@
 <template>
-    <Navbar />
+    <main>
+        <Navbar />
+        <IndexContent />
+    </main>
 </template>
 
 <script>
     import Navbar from '~/components/navbar/Navbar.vue';
+    import IndexContent from '~/components/main/IndexContent.vue';
 
     export default {
-        Navbar
+        components: {
+            Navbar,
+            IndexContent
+        }
     }
 </script>
