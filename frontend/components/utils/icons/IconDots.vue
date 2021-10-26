122b0cd7549b76c08f0e5aa4dc41d71ee79a0485
Poph-Digital <propjhh>
2021-10-26 21:36:51 +0200

    fix bg color and add dots modification messages

---
+++ frontend/components/utils/icons/IconDots.vue
@@ -0,0 +1,13 @@
+<template>
+    <Icon icon="ellipsis-h" class="text-gray-600 hover:text-black duration-500 ease-in-out cursor-pointer" />
+</template>
+
+<script>
+import Icon from '~/components/utils/icons/Icon.vue'
+
+export default {
+    'components': {
+        Icon
+    }
+}
+</script>
